8ffd2fab5a242f8e0dd1f5cb5354a8a53cd7decb
Colin Peterson <colin9416@gmail.com>
2024-12-07 23:54:21 +0000

    Handling multiple clients. Flipping single card/multiple cards & reset

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8070 });
+const playedCards = new Array();
 
 wss.on('connection', function connection(ws) {
-    const playedCards = new Array();
+
     ws.on('error', console.error);
     ws.on('message', function message(data) {
         const messages = JSON.parse(data);
         console.log('messages received: ', messages);
         for (const message of messages) {
             if (message.type === 'cardplayed') {
+                
                 playedCards.push(message.data);
             }
+
+            if (message.type == 'reset') {
+                playedCards.length = 0;
+            }
+
+            if (message.type === 'flipCard') {
+                const cardToFlip = message.data;
+                let card = playedCards.findIndex((card) => card.id === message.data.id);
+                if (card !== -1) {
+                    playedCards[card].displayed = !playedCards[card].displayed;
+                }
+            }
+
+            if (message.type === 'flipAllCards') {
+                playedCards.forEach((card) => {
+                    card.displayed = true;
+                })
+            }
         }
-        
-        ws.send(JSON.stringify(playedCards));
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(playedCards))
+            }
+        })
     });
 
     ws.send(JSON.stringify(playedCards));
